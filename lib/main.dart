745c7337202fa0e6331c4f8c18e99399873fa9e9
Jude <judeokoroafor@gmail.com>
2019-09-22 16:11:34 -0400

    worked on single randomization functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.orange,
         appBar: AppBar(
           title: Text('Roll the dice'),
           centerTitle: true,
-          backgroundColor: Colors.teal,
+          backgroundColor: Colors.orange,
+          elevation: 3.0,
         ),
         body: DicePage(),
       ),
@@ -17,26 +18,56 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber =6;
+
+  void diceRandom(){
+    setState(() {
+//      leftDiceNumber = Random().nextInt(6);
+//      rightDiceNumber = Random().nextInt(6);
+    });
+  }
+  Random leftRandom;
+  int min = 1;
+  int max = 6;
+  Random rightRandom;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 20.0, 10.0, 20.0),
-              child: Image.asset(
-                'images/dice1.png',
-              ),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 3;
+                  print('dice number = $leftDiceNumber');
+                  leftRandom = new Random();
+                  leftDiceNumber = min + leftRandom.nextInt(max - min);
+                  print("$leftDiceNumber is in the range of $min and $max");
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 20.0, 10.0, 20.0),
-              child: Image.asset(
-                'images/dice2.png',
-              ),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = 3;
+                  print('dice number = $rightDiceNumber');
+                  rightRandom = new Random();
+                  rightDiceNumber = min + rightRandom.nextInt(max - min);
+                  print("$rightDiceNumber is in the range of $min and $max");
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
         ],
@@ -44,3 +75,4 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
