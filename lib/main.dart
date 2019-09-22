00cab119d348f4a5fea990d97d54bbd977d3ae08
Jude <judeokoroafor@gmail.com>
2019-09-22 18:01:47 -0400

    worked on dual randomization functionality on click of any of the dice

--- lib/main.dart
+++ lib/main.dart
@@ -42,23 +42,52 @@ class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
         children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = 3;
-                  print('dice number = $leftDiceNumber');
-                  leftRandom = new Random();
-                  rightDiceNumber = leftMin + rightRandom.nextInt(rightMax - leftMin);
-                  leftDiceNumber = min + leftRandom.nextInt(max - min);
-                  print("Left $leftDiceNumber is in the range of $min and $max");
-                });
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDiceNumber = leftMin + rightRandom.nextInt(rightMax - leftMin);
+                      leftDiceNumber = min + leftRandom.nextInt(max - min);
+                      print("Left $leftDiceNumber is in the range of $min and $max");
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      leftDiceNumber = min + leftRandom.nextInt(max - min);
+                      rightDiceNumber = leftMin + rightRandom.nextInt(rightMax - leftMin);
+                      print("Right: $rightDiceNumber is in the range of $leftMin and $rightMax");
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDiceNumber = 6;
+                      print('dice number = $rightDiceNumber');
+                      rightRandom = new Random();
+                      leftDiceNumber = min + leftRandom.nextInt(max - min);
+                      rightDiceNumber = leftMin + rightRandom.nextInt(rightMax - leftMin);
+                      print("Right: $rightDiceNumber is in the range of $leftMin and $rightMax");
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ],
           ),
+
           Expanded(
             child: FlatButton(
               onPressed: () {
