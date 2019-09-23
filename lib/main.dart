7d6a64d952245c0b9dffc35c20e9ca986fc836c2
Jude <judeokoroafor@gmail.com>
2019-09-23 00:43:28 -0400

    Fixed the button for roll dice functionality - 3 Except center align

--- lib/main.dart
+++ lib/main.dart
@@ -27,18 +27,9 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber =6;
 
-  void diceRandom(){
-    setState(() {
-//      leftDiceNumber = Random().nextInt(6);
-//      rightDiceNumber = Random().nextInt(6);
-    });
-  }
-  Random leftRandom;
+  Random numRandom;
   int min = 1;
   int max = 7;
-  Random rightRandom;
-  int leftMin = 1;
-  int rightMax = 7;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -48,31 +39,13 @@ class _DicePageState extends State<DicePage> {
             children: <Widget>[
               Expanded(
                 child: FlatButton(
-                  onPressed: () {
-                    setState(() {
-                      leftDiceNumber = 3;
-                      print('dice number = $leftDiceNumber');
-                      leftRandom = new Random();
-                      rightDiceNumber = leftMin + rightRandom.nextInt(rightMax - leftMin);
-                      leftDiceNumber = min + leftRandom.nextInt(max - min);
-                      print("Left $leftDiceNumber is in the range of $min and $max");
-                    });
-                  },
+                  onPressed: null,
                   child: Image.asset('images/dice$leftDiceNumber.png'),
                 ),
               ),
               Expanded(
                 child: FlatButton(
-                  onPressed: () {
-                    setState(() {
-                      rightDiceNumber = 6;
-                      print('dice number = $rightDiceNumber');
-                      rightRandom = new Random();
-                      leftDiceNumber = min + leftRandom.nextInt(max - min);
-                      rightDiceNumber = leftMin + rightRandom.nextInt(rightMax - leftMin);
-                      print("Right: $rightDiceNumber is in the range of $leftMin and $rightMax");
-                    });
-                  },
+                  onPressed: null,
                   child: Image.asset('images/dice$rightDiceNumber.png'),
                 ),
               ),
@@ -83,8 +56,9 @@ class _DicePageState extends State<DicePage> {
           RaisedButton(
             onPressed: () {
               setState(() {
-                leftDiceNumber = min + leftRandom.nextInt(max - min);
-                rightDiceNumber = leftMin + rightRandom.nextInt(rightMax - leftMin);
+                numRandom = new Random();
+                leftDiceNumber = min + numRandom.nextInt(max - min);
+                rightDiceNumber = min + numRandom.nextInt(max - min);
                 print("Right: $rightDiceNumber");
                 print("Right: $leftDiceNumber");
                 print("---------------");
